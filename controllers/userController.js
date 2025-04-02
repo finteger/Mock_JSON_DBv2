e8809fdaaf4d56f6e872e2c93bfbe6b3473e77a6
finteger <nashassociatesinc@gmail.com>
2025-04-01 18:13:47 -0600

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const filePath = require('./database.json');
+const filePath = require('../database.json');
 const { readData, writeData } = require('../utils/file.js');
 
 async function createUser(req, res){
