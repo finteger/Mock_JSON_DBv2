42193a560f4bb581ec0a3abfdeff31a101c3cee8
finteger <nashassociatesinc@gmail.com>
2025-04-08 18:38:35 -0600

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,6 @@ async function createUser(req, res){
         //ternary operator
         const nextId = lastUser ? lastUser.id + 1 : 1;
 
-        console.log(req.body);
 
         //create a new user object 
         const newUser = {
@@ -28,6 +27,8 @@ async function createUser(req, res){
 
         //commits data by writing it to file.
         await writeData(data);
+
+        res.redirect('/home');
 
     } catch (error) {
         res.status(500).json(`Internal Server Error: ${error}`);
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController.js');
-
+const { readData } = require('../utils/file.js');
 
 router.use(express.json())
 router.use(express.urlencoded({extended: true}));
@@ -10,6 +10,16 @@ router.use(express.urlencoded({extended: true}));
 //Router handler for our home page
 router.get('/home',  (req, res)=>{
     res.render('home');
+});
+
+//api endpoint exposing the users resource
+router.get('/api/v1/users', async (req, res) =>{
+    try {
+        const data = await readData();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json('Internal Server Error');
+    }
 });
 
 
