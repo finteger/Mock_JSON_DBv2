d997f15be66b2f2e2d38c4149009766fb92528ee
finteger <nashassociatesinc@gmail.com>
2025-04-09 17:05:02 -0600

    Update

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
+const mongoose = require('mongoose');
+const YAML = require('yamljs');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
 const fixedWindowRateLimit = rateLimit({
  windowsMs: 1 * 15 * 1000,
  max: 10,
@@ -16,6 +22,7 @@ app.set('view engine','ejs');
 app.set('views', './views');
 
 //Middleware to serve static files
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded());
